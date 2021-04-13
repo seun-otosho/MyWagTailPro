28fe70ad37a0a73325d0f70e8f2af3670842cb1f
Seun Omotosho <seun@kolacredit.com>
2021-04-13 15:05:32 +0100

    first commit

---
+++ mywagtailpro/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'kr03@r!&9w7aoswkqib_=*xg8*3)3)g1n*1hr#&c+yb@csb(-$'
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ['*'] 
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
